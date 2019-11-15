bb4bba122054e6fc751da8c1b15b27c574c185f2
Joel Salazar Llanos <astaroth@MacBook-Pro-de-Joel-2.local>
2019-11-15 16:35:19 -0300

    Update accesos_directos.php

--- admin/application/modules/portada/controllers/accesos_directos.php
+++ admin/application/modules/portada/controllers/accesos_directos.php
@@ -14,12 +14,12 @@ class Accesos_directos extends CI_Controller {
         $this->img->min_alto_1 = 309.5;
         
         #define el tamaño de la imagen grande
-        $this->img->max_ancho_1 = 640;
-        $this->img->max_alto_1 = 640;
+        $this->img->max_ancho_1 = 960;
+        $this->img->max_alto_1 = 960;
         
         #define el tamaño del recorte
-        $this->img->recorte_ancho_1 = 640;
-        $this->img->recorte_alto_1 = 619;
+        $this->img->recorte_ancho_1 = 960;
+        $this->img->recorte_alto_1 = 929;
         
         #imagen 2
         #define el tamaño del contenedor en la vista
