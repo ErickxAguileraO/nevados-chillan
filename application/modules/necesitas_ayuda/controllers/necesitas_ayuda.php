0a355f8c0ff4506a80b11eb5a515648d619bd67c
Victor Cea <vicea@aeurus.cl>
2020-11-18 16:45:14 -0300

    prueba 2 de descarga de archivo

--- application/modules/necesitas_ayuda/controllers/necesitas_ayuda.php
+++ application/modules/necesitas_ayuda/controllers/necesitas_ayuda.php
@@ -374,9 +374,10 @@ public function descargar_archivo(){
 
 			$codigo = $this->uri->segment(4);
       $archivo = $this->ws->obtener(58,"cor_codigo = $codigo");
-      print_array($archivo);die;
+      print_array($archivo);#die;
        $this->load->helper('download');
        $name = basename($archivo->archivo_adjunto);
+        print_array($name);die;
        $data = file_get_contents(URL_ADMINISTRACION.$archivo->archivo_adjunto);
 
        force_download($name, $data);
