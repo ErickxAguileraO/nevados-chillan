f6a47eaaada9713b63ff0eb576e3cc7d8f9c8c7c
unknown <bcoloma@aeurus.cl>
2023-04-11 16:09:13 -0400

    footer nuevo 2

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,71 @@
+<footer>
+<style>
+    .reserva-aqui{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: fixed;
+      z-index: 100;
+      bottom: 1.48rem;
+      left: 25.48rem;
+      width: 180px;
+      height: 48px;
+      border-radius: 50px;
+      background-color: #004e90;
+      color: white;
+      text-decoration: none !important;
+      box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%), 0 1px 18px 0 rgb(0 0 0 / 12%);
+      gap: 5px;
+    }
+    .reserva-aqui p{
+      margin-top: 3px;
+      font-weight: 700;
+    }
+    @media (max-width: 500px) {
+      .reserva-aqui{
+        bottom: 7rem;
+        left: 1.48rem;
+      }
+    }
+  </style>
+  <a href="https://contenidos.nevadosdechillan.art2fly.com/cgi-bin/paso1.cgi" target="blank_" class="reserva-aqui">
+    <img src="/imagenes/template/reserva.png" width="24px" height="24px"/>
+    <p>Reserva Aquí</p>
+  </a>
+  <?php if(($home_indicador)){ ?>
+  <!-- Inicio Auspiciadores -->
+  <?php if(count($auspiciadores)>0){ ?>
+  <section class="auspiciadores">
+    <h1>PARTNERS</h1>
+    <div class="auspiciadores-background">
+      <div class="carrusel partners-responsive">
+        <?php foreach($auspiciadores as $item): ?>
+        <div>
+
+        <?php 
+        
+     
+        if($item->link){ ?>
+          <a target="_blank" href="<?php echo $item->link; ?>">
+        <?php } ?>
+        
+        <figure><img alt="<?=$item->nombre?>" src="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>" />
+        </figure>
+        
+        <?php if($item->link){ ?>
+          </a>
+        <?php } ?> 
+        
+        </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </section>
+  <?php } ?> 
+  <!-- Fin Auspiciadores -->
+  <?php } ?>
+
+</footer>
 <footer class="footer-nuevo">
     <div class="suscribirse-nuevo">
       <h3>Suscríbete para recibir alertas y novedades</h3>
