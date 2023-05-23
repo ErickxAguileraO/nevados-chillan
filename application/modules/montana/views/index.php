e0abe9125845c58027b99a29757543ba5ef6f476
Natty <nasenjo@aeurus.cl>
2023-05-23 17:20:29 -0400

    Maqueta Mapa de pistas

--- application/modules/montana/views/index.php
+++ application/modules/montana/views/index.php
@@ -1,356 +1,60 @@
-<!-- inicio Slider -->
-<div class="flexslider">
-<?php if(count($sliders)>0){ ?>
-  <ul class="slides">
-    <?php foreach($sliders as $item): ?>
-    <li style="background: url(<?=URL_ADMINISTRACION.$item->imagen_adjunta?>) no-repeat;">
-      <div class="center">
-      <?php if($item->url_video){ ?>
-      <div class="texto-slider float-left">
-        <?php if($item->link){ ?>
-          <a href="<?=$item->link?>" title="<?=$item->titulo?>">
-        <?php }?>
-        <span><?=$item->antetitulo?></span>
-        <?php if($item->link){ ?>
-          </a>
-        <?php }?>
-        <h2><?=$item->titulo?></h2>
-        <p><?=$item->bajada?></p>
-      </div>
-      <div class="video-slider float-right">
-        <iframe width="100%" height="300px" src="<?=str_replace('watch?v=','embed/',$item->url_video)?>" frameborder="0" allowfullscreen></iframe>
-      </div>
-      <?php }else{ ?>
-        <div class="full-texto-slider float-left">
-          <?php if($item->link){ ?>
-            <a href="<?=$item->link?>" title="<?=$item->titulo?>">
-          <?php }?>
-          <span><?=$item->antetitulo?></span>
-          <?php if($item->link){ ?>
-            </a>
-          <?php }?>
-          <h2><?=$item->titulo?></h2>
-          <p><?=$item->bajada?></p>
-        </div>
-      <?php } ?>
-      </div>
-    </li>
-  <?php endforeach; ?>
-  </ul>
-<?php }?>
+<div class="banner-top">
+  <figure><img src="/imagenes/sitio/banner-montana.jpg" width="1920" height="406" />
+    <figcaption class="center"> <span>MAPA DE PISTAS</span></figcaption>
+  </figure>
 </div>
-<!-- Fin Slider -->
 <div class="center">
   <?=$this->layout->getNav();?>
-  <div class="intro">
-      <?php echo ($introduccion) ? html_entity_decode($introduccion->descripcion) : ''; ?>
-      <!--
-    <h2>Parque de Agua termal</h2>
-    <p>Parque de Aguas es en sí, un atractivo turístico único.</p>
-<p>Ubicado a un costado del Hotel Alto Nevados, Parque de Aguas es un centro termal y recreativo donde se une el relajo de las aguas termales con la entretención, música, gastronomía y barra con variados jugos y tragos.</p>
-<p>El lugar cuenta con 4 piscinas termales de diferentes tamaños y temperaturas, en donde se pueden vivir distintas experiencias. Es así como en la piscina principal se puede conjugar el baño termal con el relajo de un pisco sour en su bar insertado dentro del agua. Por su lado, si prefiere una experiencia de silencio y relajo hay piscinas mas pequeñas y alejadas que le brindarán el espacio y sensación de paz que también se busca en la montaña.</p>
-<p>Parque de Aguas cuenta con un restaurant, en donde se puede disfrutar de diferentes platos y preparaciones, algunos de comida rápida y otros gourmet para que usted viva su experiencia a su medida.</p>
-<p>Los servicios incluyen camarines y un spa, generando en su conjunto un lugar en donde se puede vivir un gran día.</p>
-  -->
+  <section class="intro">
+    <h1>Mapa de pistas</h1>
+    <p>El BikePark funciona desde las 10:00 hasta las 16:45. La apertura parte siempre por el andarivel Tata y progresivamente el resto. El cierre parte por el andarivel Refugio y luego progresivamente el resto.<br />
+      <strong>*Disclosure</strong>: Las condiciones climáticas como visibilidad, viento, lluvia y tipo de nevada pueden cambiar drásticamente, en pocos minutos, y no quedar reflejadas, en un cierto intervalo, en este reporte. Más acá.</p>
+  </section>
+  <div class="contenedor cont-secciones">
+    <figure class="picture"> <img src="/imagenes/borrar/mapa.jpg" width="679" height="408" alt="Mapa" /> </figure>
+    <div class="txt-block-contenido center-home">
+      <h1>Valle Hermoso</h1>
+      <p>Se encuentra ubicado a 80 km de la ciudad de Chillán y es el primer recinto del grupo Nevados de Chillán.</p>
+      <p>Situado en un lugar excepcional rodeado de naturaleza y a la sombra de un bosque. Ofrece 3 piscinas termales al aire libre, un restaurante, un mini-market, sectores habilitados para asados y picnic y departamentos completamente equipados, lo que lo convierte en una opción segura y preferida para aquellos que buscan experiencias termales familiares.</p>
+      <a class="ver-mas" href="#">Leer más</a> </div>
   </div>
-      <!-- <a href="https://waze.to/lr/h63mnp1f4m" class="wase-btn" target="_blank" style="margin-top:50px;"><img src="../../imagenes/template/wase-btn.jpg"></a> -->
-
-  <div class="clear"></div>
-  <?php /*?><!-- Inicio servicios asociados -->
-  <?php if(count($servicios)>0){ ?>
-  <div class="serv-compl wow fadeInLeft" id="servicios">
-    <h2>Servicios Complementarios</h2>
-    <small>Estos servicios tienen un valor adicional</small>
-    <ul>
-      <?php foreach($servicios as $item): ?>
-      <li><img alt="<?=$item->nombre?>" src="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>" />
-        <h3><?=$item->nombre?></h3>
-      </li>
-    <?php endforeach;?>
-    </ul>
-    <div class="clear"></div>
+  <div class="contenedor cont-secciones">
+    <figure class="picture"> <img src="/imagenes/borrar/mapa.jpg" width="679" height="408" alt="Mapa" /> </figure>
+    <div class="txt-block-contenido center-home">
+      <h1>Valle Hermoso</h1>
+      <p>Se encuentra ubicado a 80 km de la ciudad de Chillán y es el primer recinto del grupo Nevados de Chillán.</p>
+      <p>Situado en un lugar excepcional rodeado de naturaleza y a la sombra de un bosque. Ofrece 3 piscinas termales al aire libre, un restaurante, un mini-market, sectores habilitados para asados y picnic y departamentos completamente equipados, lo que lo convierte en una opción segura y preferida para aquellos que buscan experiencias termales familiares.</p>
+      <a class="ver-mas" href="#">Leer más</a> </div>
   </div>
-  <?php } ?>
-  <!--Fin Servicios asociados --><?php */?>
+  <div class="contenedor cont-secciones">
+    <figure class="picture"> <img src="/imagenes/borrar/mapa.jpg" width="679" height="408" alt="Mapa" /> </figure>
+    <div class="txt-block-contenido center-home">
+      <h1>Valle Hermoso</h1>
+      <p>Se encuentra ubicado a 80 km de la ciudad de Chillán y es el primer recinto del grupo Nevados de Chillán.</p>
+      <p>Situado en un lugar excepcional rodeado de naturaleza y a la sombra de un bosque. Ofrece 3 piscinas termales al aire libre, un restaurante, un mini-market, sectores habilitados para asados y picnic y departamentos completamente equipados, lo que lo convierte en una opción segura y preferida para aquellos que buscan experiencias termales familiares.</p>
+      <a class="ver-mas" href="#">Leer más</a> </div>
+  </div>
+  <div class="contenedor cont-secciones">
+    <figure class="picture"> <img src="/imagenes/borrar/mapa.jpg" width="679" height="408" alt="Mapa" /> </figure>
+    <div class="txt-block-contenido center-home">
+      <h1>Valle Hermoso</h1>
+      <p>Se encuentra ubicado a 80 km de la ciudad de Chillán y es el primer recinto del grupo Nevados de Chillán.</p>
+      <p>Situado en un lugar excepcional rodeado de naturaleza y a la sombra de un bosque. Ofrece 3 piscinas termales al aire libre, un restaurante, un mini-market, sectores habilitados para asados y picnic y departamentos completamente equipados, lo que lo convierte en una opción segura y preferida para aquellos que buscan experiencias termales familiares.</p>
+      <a class="ver-mas" href="#">Leer más</a> </div>
+  </div>
+  <section style="padding-top: 80px;">
+  <h1 class="text-center clear" style=" margin-bottom: 46px;">Cámaras en vivo</h1>
+  <div class="listado-noticias camaras">
+    <article>
+      <figure><img src="/imagenes/borrar/noticias.jpg" alt="Título de la noticia" width="440" height="200"></figure>
+      <h3>Cámara 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.</p>
+      <span class="ver-mas"><a href="#">Ver más</a></span> </article>
+    <article>
+      <figure><img src="/imagenes/borrar/noticias.jpg" alt="Título de la noticia" width="440" height="200"></figure>
+      <h3>Cámara 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.</p>
+      <span class="ver-mas"><a href="#">Ver más</a></span> </article>
+  </div>
+  </section>
 </div>
-<div class="valores-seccion" id="valores">
-    <div class="block-table">
-      <div class="center">
-        <?php if($programas){           
-            foreach($programas as $pro) {           
-          ?>
-          <div class="block-tr">
-            <div class="block-th">
-              <h3><?=$pro->titulo?></h3>
-              <ul>
-                <li><?=$pro->bajada_uno?></li>
-                <li><?=$pro->bajada_dos?></li>
-              </ul>
-            </div>
-
-              <?php foreach($pro->opciones as $op ){ ?>
-            <div class="block-td">
-              <h4><?=$op->nombre?></h4>
-              <span class="valor">$<?=$op->monto?></span> <span class="condiciones"><?=$op->resumen?></span> 
-            </div>
-              <?php } ?>
-
-          </div>
-          <?php
-            }
-          }
-          
-          ?>
-
-      </div>
-    </div>
-  
-
-<!--
-<div class="seccion-fondo valores-seccion" style="background: none !important;" id="valores">
-  <h2 style="color: #00274c;">Programas y valores</h2>
-  <div class="center">
-    <?php $cant = explode("<table",$valores->contenido); $cant = count($cant)-1;
-    if($cant == 1){
-      echo '<style>
-      .valores-seccion table{width:99%;}
-      </style>';
-    }
-    if($cant == 2){
-      echo '<style>
-      .valores-seccion table{width:49%;}
-      </style>';
-    }
-    if($cant == 3){
-      echo '<style>
-      .valores-seccion table{width:32%;}
-      </style>';
-    }
-    ?>
-  <?=$valores->contenido?>
-  <div class="clear"></div>
-  </div>
-</div>
---><!-- inicio secciones -->
-<?php if(count($secciones)>0){ ?>
-<?php foreach($secciones as $item): ?>
-
-<!--Posicion foto derecha-->
-<?php if($item->posicion == 0) { ?>
-<?php if($item->tipo_de_imagen==2){ ?>
-<div class="center cont2 wow fadeInLeft" id="<?=$item->url?>"><!-- imagen chica -->
-  <div class="texto float-right">
-    <h2 style="color:#033e6c;"><?=$item->titulo?></h2>
-    <p style="color:#004a8e;"><?=$item->bajada?></p>
-    <!-- si es un enlace -->
-    <?php if($item->link){ ?>
-    <span><a href="<?=$item->link?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link?></a></span>
-    <?php } ?>
-    <?php if($item->link_2){ ?>
-    <span><a href="<?=$item->link_2?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link_2?></a></span>
-    <?php } ?>
-    <?php if($item->link_3){ ?>
-    <span><a href="<?=$item->link_3?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link_3?></a></span>
-    <?php } ?>
-    <!-- si es imagen -->
-    <?php if($item->imagen_adjunta){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>">
-    <?=$item->nombre_imagen_adjunta?></a></span>
-    <?php } ?>
-    <?php if($item->imagen_adjunta2){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta2?>">
-    <?=$item->nombre_imagen_adjunta2?></a></span>
-    <?php } ?>
-    <?php if($item->imagen_adjunta3){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta3?>">
-    <?=$item->nombre_imagen_adjunta3?></a></span>
-    <?php } ?>
-  </div>
-  <?php if($item->embed_video == "") { ?>
-  <?php if(count($item->galeria)>1){ ?>
-  <div class="slider float-left wow fadeInRight">
-    <div class="slider-habitacion">
-      <ul class="slides">
-        <?php foreach($item->galeria as $imagen): ?>
-        <li> <img src="<?=URL_ADMINISTRACION.$imagen->ruta_interna?>" /> </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
-  <?php }else{ ?>
-  <div class="slider float-left wow fadeInLeft"> <img src="<?=URL_ADMINISTRACION.$item->galeria[0]->ruta_interna?>" /> </div>
-  <?php } ?>
-  <?php } else { ?>
-  <div class="video-slider float-left"> <?php echo html_entity_decode($item->embed_video);?> </div>
-  <?php } ?>
-  <div class="clear"></div>
-</div>
-<?php }else{ ?>
-<div class="seccion-fondo ski-seccion" style="background-image: url(<?=URL_ADMINISTRACION.$item->imagen_adjunta_fondo?>) !important;" id="<?=$item->url?>"><!-- imagen grande -->
-  <div class="center">
-    <div class="texto wow fadeInLeft">
-      <h2><?=$item->titulo?></h2>
-      <p><?=$item->bajada?></p>
-      <!-- si es un enlace -->
-      <?php if($item->link){ ?>
-      <span><a  href="<?=$item->link?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link?>
-      </a></span>
-      <?php } ?>
-      <?php if($item->link_2){ ?>
-      <span><a  href="<?=$item->link_2?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link_2?>
-      </a></span>
-      <?php } ?>
-      <?php if($item->link_3){ ?>
-      <span><a  href="<?=$item->link_3?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link_3?>
-      </a></span>
-      <?php } ?>
-      <!-- si es imagen -->
-      <?php if($item->imagen_adjunta){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>">
-      <?=$item->nombre_imagen_adjunta?>
-      </a></span>
-      <?php } ?>
-      <?php if($item->imagen_adjunta2){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta2?>">
-      <?=$item->nombre_imagen_adjunta2?>
-      </a></span>
-      <?php } ?>
-      <?php if($item->imagen_adjunta3){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta3?>">
-      <?=$item->nombre_imagen_adjunta3?>
-      </a></span>
-      <?php } ?>
-    </div>
-  </div>
-  <div class="clear"></div>
-</div>
-<?php } ?>
-<?php } else { ?>
-
-<!--POSICION FOTO IZQUIERDA-->
-<?php if($item->tipo_de_imagen==2){ ?>
-<div class="center cont2 wow fadeInLeft" id="<?=$item->url?>"><!-- imagen chica -->
-  <div class="texto">
-    <h2 style="color:#033e6c;"><?=$item->titulo?></h2>
-    <p  style="color:#004a8e;"><?=$item->bajada?></p>
-    <!-- si es un enlace -->
-    <?php if($item->link){ ?>
-    <span><a href="<?=$item->link?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link?>
-    </a></span>
-    <?php } ?>
-    <?php if($item->link_2){ ?>
-    <span><a href="<?=$item->link_2?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link_2?>
-    </a></span>
-    <?php } ?>
-    <?php if($item->link_3){ ?>
-    <span><a href="<?=$item->link_3?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-    <?=$item->nombre_link_3?>
-    </a></span>
-    <?php } ?>
-    <!-- si es imagen -->
-    <?php if($item->imagen_adjunta){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>"><?=$item->nombre_imagen_adjunta?></a></span>
-    <?php } ?>
-    <?php if($item->imagen_adjunta2){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta2?>"><?=$item->nombre_imagen_adjunta2?></a></span>
-    <?php } ?>
-    <?php if($item->imagen_adjunta3){ ?>
-    <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta3?>"><?=$item->nombre_imagen_adjunta3?></a></span>
-    <?php } ?>
-  </div>
-  <?php if($item->embed_video == "") { ?>
-  <?php if(count($item->galeria)>1){ ?>
-  <div class="slider float-right wow fadeInRight">
-    <div class="slider-habitacion">
-      <ul class="slides">
-        <?php foreach($item->galeria as $imagen): ?>
-        <li> <img src="<?=URL_ADMINISTRACION.$imagen->ruta_interna?>" /> </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
-  <?php }else{ ?>
-  <img src="<?=URL_ADMINISTRACION.$item->galeria[0]->ruta_interna?>" class="wow fadeInRight" />
-  <?php } ?>
-  <?php } else { ?>
-  <div class="video-slider float-right"> <?php echo $item->embed_video;?> </div>
-  <?php } ?>
-  <div class="clear"></div>
-</div>
-<?php }else{ ?>
-<div class="seccion-fondo ski-seccion" style="background-image: url(<?=URL_ADMINISTRACION.$item->imagen_adjunta_fondo?>) !important;" id="<?=$item->url?>"><!-- imagen grande -->
-  <div class="center">
-    <div class="texto wow fadeInLeft">
-      <h2><?=$item->titulo?></h2>
-      <p><?=$item->bajada?></p>
-      <!-- si es un enlace -->
-      <?php if($item->link){ ?>
-      <span><a  href="<?=$item->link?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link?></a></span>
-      <?php } ?>
-      <?php if($item->link_2){ ?>
-      <span><a  href="<?=$item->link_2?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link_2?></a></span>
-      <?php } ?>
-      <?php if($item->link_3){ ?>
-      <span><a  href="<?=$item->link_3?>"> <img src="/imagenes/template/arrow.png" class="arrow" />
-      <?=$item->nombre_link_3?></a></span>
-      <?php } ?>
-      <!-- si es imagen -->
-      <?php if($item->imagen_adjunta){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta?>"><?=$item->nombre_imagen_adjunta?></a></span>
-      <?php } ?>
-      <?php if($item->imagen_adjunta2){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta2?>"><?=$item->nombre_imagen_adjunta2?></a></span>
-      <?php } ?>
-      <?php if($item->imagen_adjunta3){ ?>
-      <span><a class="venobox" data-gall="myGallery" data-title="Titulo para imagen" href="<?=URL_ADMINISTRACION.$item->imagen_adjunta3?>"><?=$item->nombre_imagen_adjunta3?></a></span>
-      <?php } ?>
-    </div>
-  </div>
-  <div class="clear"></div>
-</div>
-<?php } ?>
-<?php } ?>
-<?php endforeach; ?>
-<?php }?>
-<!-- Fin Secciones --> 
-
-<!-- Inicio Banners -->
-<?php  if(count($banners)>0){ ?>
-    <div class="center eventos-corporativos">
-        <a href="<?= $banners[0]->link?>"> <img src="<?=URL_ADMINISTRACION.$banners[0]->imagen_adjunta?>" class="float-left" /></a>
-        <a href="<?= $banners[1]->link?>"><img src="<?=URL_ADMINISTRACION.$banners[1]->imagen_adjunta?>" class="float-right" /></a>
-    </div>
-<?php } ?>
-<!-- Fin Banners -->
-
-
-
-<script type="text/javascript">
-$(document).ready(function(){
-    $('.venobox').venobox();
-});
-    $(window).load(function(){
-      $('.flexslider').flexslider({
-        animation: "fade",
-		slideshowSpeed: 5000,
-		directionNav: false,
-      });
-
-      $('.slider-habitacion').flexslider({
-        animation: "slide",
-		slideshowSpeed: 5000,
-		directionNav: false,
-      });
- });
-
-</script>
