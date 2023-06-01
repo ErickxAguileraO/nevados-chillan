a90f97cca7cee2dae3e8e2a707f3c05b4175bc97
crojas <crojas@aeurus.cl>
2023-06-01 15:28:48 -0400

    Listado mapas
    Se agregó el listado de mapas con paginación.

--- admin/application/modules/invierno/views/mapa_pistas/index.php
+++ admin/application/modules/invierno/views/mapa_pistas/index.php
@@ -23,8 +23,44 @@
 			</div>
         </div>
     </form>
+
     <div class="subtitulo-btn">
     	<h2>&nbsp;</h2>
         <a class="btn btn-default" href="/invierno/mapa-pistas/agregar/">Agregar mapa</a>
     </div>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th class="col-xs-11">Nombre</th>
+                <th colspan="2" class="text-center">Opciones</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <?php if ($mapas) { ?>
+                <?php foreach($mapas as $mapa){ ?>
+                    <tr> 
+                        <td><?= $mapa->nombre; ?></td>
+						<td class="text-center">
+                            <a href="invierno/mapa-pistas/editar/<?= $mapa->codigo; ?>/">
+                                <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                            </a>
+                        </td>
+                        <td class="text-center">
+							<a rel="<?= $mapa->codigo; ?>" class="eliminar" style="cursor:pointer;">
+								<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+							</a>
+						</td>
+                    </tr>
+                <?php } ?>
+            <?php } else{ ?>
+                <tr>
+                    <td colspan="4" style="text-align: center;"><i>No hay mapas registrados.</i></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+    <?= $pagination; ?>
 </div>
